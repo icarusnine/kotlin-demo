c32940d2a77929432af6d9638c7e0993525d908c
icarusnine <haruru@daum.net>
2018-11-08 21:34:16 +0900

    Add Event Entity

---
+++ src/main/kotlin/spring/boot/kotlindemo/event/Event.kt
@@ -0,0 +1,25 @@
+package spring.boot.kotlindemo.event
+
+import java.time.LocalDateTime
+
+data class Event(
+        var id: Int,
+        var name: String,
+        var description: String,
+        var beginEnrollmentDateTime: LocalDateTime,
+        var closeEnrollmentDateTime: LocalDateTime,
+        var beginEventDateTime: LocalDateTime,
+        var endEventDateTime: LocalDateTime,
+        var location: String, // (optional) 이게 없으면 온라인 모임
+        var basePrice: Int,
+        var maxPrice: Int,
+        var limitOfEnrollment: Int,
+        var offline: Boolean,
+        var free: Boolean,
+        var eventStatus: EventStatus = EventStatus.DRAFT
+
+)
+
+enum class EventStatus {
+    DRAFT, PUBLISHED, BEGAN_ENROLLMEND, CLOSED_ENROLLMENT, STARTED, ENDED
+}
---
+++ src/test/kotlin/spring/boot/kotlindemo/event/EventTest.kt
@@ -0,0 +1,32 @@
+package spring.boot.kotlindemo.event
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+import java.time.LocalDateTime
+
+class EventTest {
+    @Test
+    fun create() {
+        //Given
+        val event = Event(
+                id = 1,
+                name = "Spring Meetup",
+                description = "Spring boot meetup in suwomn",
+                beginEnrollmentDateTime = LocalDateTime.of(2018, 11, 8, 10, 0),
+                closeEnrollmentDateTime = LocalDateTime.of(2018, 11, 9, 10, 0),
+                beginEventDateTime = LocalDateTime.of(2018, 11, 10, 10, 0),
+                endEventDateTime = LocalDateTime.of(2018, 11, 11, 10, 0),
+                location = "Suwon",
+                basePrice = 100,
+                maxPrice = 200,
+                limitOfEnrollment = 10,
+                offline = false,
+                free = false
+        )
+
+        //When & Then
+        assertThat(event.basePrice).isEqualTo(100)
+        assertThat(event.maxPrice).isEqualTo(200)
+        assertThat(event.name).isEqualTo("Spring Meetup")
+    }
+}
