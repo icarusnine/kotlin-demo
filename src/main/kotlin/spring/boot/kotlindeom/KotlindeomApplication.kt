0efb6828a59fbd5944ee0255b58c21d7797a59e4
icarusnine <haruru@daum.net>
2018-11-08 20:48:34 +0900

    Initial commit spring boot + kotiln - web, hateoas, jpa, h2, restdocs

---
+++ src/main/kotlin/spring/boot/kotlindeom/KotlindeomApplication.kt
@@ -0,0 +1,11 @@
+package spring.boot.kotlindeom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlindeomApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlindeomApplication>(*args)
+}
